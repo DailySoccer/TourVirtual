bec1f229fa24fdcc458ea381ebf990a3582b60e4
sreveloc <sreveloc@gmail.com>
2016-01-22 12:04:01 +0100

    Loading bar en Loading Screen

---
+++ Assets/Scripts/UI/LoadingBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class LoadingBar : MonoBehaviour {
+
+	public Image fillBar;
+	public Text description;
+	public int MaxLoad = 100;
+	public int CurrentLoad = 10;
+
+	public void SetValue(int value) { 
+		value = Mathf.Clamp (value, 0, MaxLoad);
+	}
+
+	public void SetValue(int value, string texto) { 
+		SetValue (value);
+		description.text = texto;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (fillBar != null) {
+			fillBar.fillAmount = (float) CurrentLoad / (float) MaxLoad;
+		}
+	}
+
+}
