e4366d634f8d8c7d32f9a9f86697d405e8d4cf64
Fernando Perez <gunderwulde@hotmail.es>
2016-01-25 10:48:26 +0100

    - Namespacing las clases de mini juego de basket.

--- Assets/Scripts/Minigames/Basket/GoalArea.cs
+++ Assets/Scripts/Minigames/Basket/GoalArea.cs
@@ -1,34 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoalArea : MonoBehaviour {
-	public ParticleSystem psStar;
-    public Shooter shooter;
+namespace Basket
+{
 
-    // Use this for initialization
-    void Start(){
+    public class GoalArea : MonoBehaviour
+    {
+        public ParticleSystem psStar;
+        public Shooter shooter;
 
-    }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
 
 #if UNITY_EDITOR
-    void Update(){
-        if( Input.GetKeyDown(KeyCode.Return))
-            OnGoal();
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+                OnGoal();
 
-    }
+        }
 #endif
 
-    void OnTriggerEnter (Collider other) {
-		ShotBall sb = other.GetComponent<ShotBall>();
-		if (sb != null) {
-            other.name = "goal";
-            OnGoal();
+        void OnTriggerEnter(Collider other)
+        {
+            ShotBall sb = other.GetComponent<ShotBall>();
+            if (sb != null)
+            {
+                other.name = "goal";
+                OnGoal();
+            }
         }
-	}
 
-    void OnGoal() {
-        // Goal!!
-        psStar.Play();
-        if (shooter != null) shooter.OnScore();
+        void OnGoal()
+        {
+            // Goal!!
+            psStar.Play();
+            if (shooter != null) shooter.OnScore();
+        }
     }
 }
--- Assets/Scripts/Minigames/Basket/ShotBall.cs
+++ Assets/Scripts/Minigames/Basket/ShotBall.cs
@@ -1,20 +1,18 @@
-﻿/* ShotBall.cs
-  version 1.0 - August 7, 2015
-
-  Copyright (C) 2015 Wasabi Applications Inc.
-   http://www.wasabi-applications.co.jp/
-*/
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class ShotBall : MonoBehaviour {
+namespace Basket
+{
+    public class ShotBall : MonoBehaviour
+    {
 
 
-	public bool isActive {get; private set;}	
+        public bool isActive { get; private set; }
 
 
-	public void ChangeActive () {
-		isActive = true;
-	}
+        public void ChangeActive()
+        {
+            isActive = true;
+        }
+    }
 }
