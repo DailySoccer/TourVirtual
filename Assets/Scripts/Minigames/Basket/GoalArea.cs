7ec2552d52ec3b1d10398c90ab3e5ca9c3bb2609
Fernando Perez <gunderwulde@hotmail.es>
2016-01-25 08:51:37 +0100

    - Mini juego de basket.

---
+++ Assets/Scripts/Minigames/Basket/GoalArea.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalArea : MonoBehaviour {
+	public ParticleSystem psStar;
+    public Shooter shooter;
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+#if UNITY_EDITOR
+    void Update(){
+        if( Input.GetKeyDown(KeyCode.Return))
+            OnGoal();
+
+    }
+#endif
+
+    void OnTriggerEnter (Collider other) {
+		ShotBall sb = other.GetComponent<ShotBall>();
+		if (sb != null) {
+            other.name = "goal";
+            OnGoal();
+        }
+	}
+
+    void OnGoal() {
+        // Goal!!
+        psStar.Play();
+        if (shooter != null) shooter.OnScore();
+    }
+}
