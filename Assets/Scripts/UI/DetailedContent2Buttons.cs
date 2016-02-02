6ee0937caa500ce4318c9fa2a06a05ddfde98d32
sreveloc <sreveloc@gmail.com>
2016-02-02 12:41:44 +0100

    repaso modales

---
+++ Assets/Scripts/UI/DetailedContent2Buttons.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum DetailedContent2ButtonsLayout {
+	GOTOSHOP,
+	BUYITEM,
+	SHARE
+}
+
+[ExecuteInEditMode]
+public class DetailedContent2Buttons : MonoBehaviour {
+
+	public Text TheName;
+	public Image ThePicture;
+	public GameObject CancelButton;
+	public GameObject GotoShopButton;
+	public GameObject BuyButton;
+	public GameObject ShareButton;
+
+	public DetailedContent2ButtonsLayout CurrentLayout;
+
+	private DetailedContent2ButtonsLayout LastLayout;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (LastLayout != CurrentLayout) {
+			switch (CurrentLayout) {
+			case DetailedContent2ButtonsLayout.BUYITEM:
+				CancelButton.SetActive(true);
+				BuyButton.SetActive(true);
+				GotoShopButton.SetActive(false);
+				ShareButton.SetActive(false);
+				break;
+			case DetailedContent2ButtonsLayout.GOTOSHOP:
+				CancelButton.SetActive(true);
+				BuyButton.SetActive(false);
+				GotoShopButton.SetActive(true);
+				ShareButton.SetActive(false);
+				break;
+			case DetailedContent2ButtonsLayout.SHARE:
+				CancelButton.SetActive(false);
+				BuyButton.SetActive(false);
+				GotoShopButton.SetActive(false);
+				ShareButton.SetActive(true);
+				break;
+			}
+			LastLayout = CurrentLayout;
+		}
+	}
+}
