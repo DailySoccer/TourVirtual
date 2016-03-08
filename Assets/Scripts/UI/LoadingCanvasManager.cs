d899fc75d2d188d96b99edbaf26b8c5717ca44c8
Fernando Perez <gunderwulde@hotmail.es>
2016-03-08 16:31:42 +0100

    - Sistema para mostrar y cocultar el LoadingCanvas.

---
+++ Assets/Scripts/UI/LoadingCanvasManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LoadingCanvasManager : MonoBehaviour {
+
+    public static LoadingCanvasManager Instance { get; private set; }
+    void Awake () {
+        Instance = this;
+        Instance.gameObject.SetActive(false);
+    }
+
+    public static void Show() {
+        Instance.gameObject.SetActive(true);
+    }
+
+    public static void Hide() {
+        Instance.gameObject.SetActive(false);
+    }
+
+}
