e5e1a059d4ec81a0d1773763087caceafe6aae86
Fernando Perez <gunderwulde@hotmail.es>
2016-03-22 12:46:38 +0100

    - Cambio de cover. - Control drag rotation. - Mensaje de error. - Cambio en cover.

---
+++ Assets/Scripts/Utils/RotateDrag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RotateDrag : MonoBehaviour
+{
+    float oldPoint;
+    void OnMouseDown()
+    {
+        oldPoint = Input.mousePosition.x / Screen.width;
+    }
+
+    void OnMouseDrag()
+    {
+        var newPoint = Input.mousePosition.x / Screen.width;
+        float dif = (oldPoint- newPoint) * 360.0f;
+        transform.Rotate(Vector3.up, dif);
+
+        oldPoint = newPoint;
+    }
+}
