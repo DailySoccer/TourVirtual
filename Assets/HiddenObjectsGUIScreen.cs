bae25c960c6e8881b6b55cdd95bcce2dcb596e3b
sreveloc <sreveloc@gmail.com>
2016-04-07 21:10:45 +0200

    minigame hidden bjects: funcionalidad

---
+++ Assets/HiddenObjectsGUIScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HiddenObjectsGUIScreen : GUIScreen {
+
+	public MinigameTimerHUD TheTimer;
+	public Text TxTScore;
+
+
+	//Arrastramos aqui el mainmanager
+	public  HiddenObjects.HiddenObjects minigameController;
+	
+	// Use this for initialization
+	void Awake () {
+	}
+
+	public void SetupTheHUD() {
+		//minigameController = MainManager.Instance.GetComponent<HiddenObjects.HiddenObjects> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		TxTScore.text = minigameController.numFoundObjects + " / " + minigameController.numHiddenObjects;
+		TheTimer.TimeLeft = 1 - (minigameController.RemaingTime / minigameController.maxTime);
+	}
+}
---
+++ Assets/HiddenObjectsGameCanvasController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenObjectsGameCanvasController : MonoBehaviour {
+
+	public GuiMinigameScreen HiddenObjectsMinigameHUD;
+	
+	public GUIPopUpScreen ModalHiddenObjectsGameScreen;
+
+	GameCanvasManager gcm;
+
+	// Use this for initialization
+	void Awake () {
+		gcm = GameObject.FindGameObjectWithTag ("GameCanvasManager").GetComponent<GameCanvasManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+	public void StartHiddenObjectsGame() {
+		gcm.ShowScreen (HiddenObjectsMinigameHUD);
+		ModalHiddenObjectsGameScreen.IsOpen = true;
+	}
+}
