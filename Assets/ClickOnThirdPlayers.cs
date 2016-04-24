2f90ae4edc5b1edc4f2ccb65d55adf6e05a1e6e8
sreveloc <sreveloc@gmail.com>
2016-04-24 03:50:37 +0200

    bugfix ver perfil de terceros através de chat --> Fixed

---
+++ Assets/ClickOnThirdPlayers.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnThirdPlayers : MonoBehaviour {
+	int layerMask;
+	GameCanvasManager gcm;
+	// Use this for initialization
+	void Start () {
+		layerMask = LayerMask.GetMask("Net");
+		gcm = GameObject.FindGameObjectWithTag ("GameCanvasManager").GetComponent<GameCanvasManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (gcm.currentGUIPopUpScreen == null) {
+			// Moviles
+			if (Input.touchCount != 0) {
+				Touch touch = Input.touches [0];
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				RaycastHit hit;
+			
+				if (Physics.Raycast (ray, out hit, 20.0f, layerMask)) {
+					if (hit.collider.gameObject.name.Contains ("base"))
+						hit.collider.gameObject.GetComponent<RemotePlayerHUD> ().RemotePlayerHUD_ClickHandle ();
+				}
+			}
+			// Editor Unity
+			if ( Input.GetMouseButtonDown(0) ) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if ( Physics.Raycast(ray, out hit, 20.0f, layerMask) ){
+					//if (hit.collider.gameObject.name.Contains("clone"))
+						hit.collider.gameObject.GetComponentInChildren<RemotePlayerHUD>().RemotePlayerHUD_ClickHandle();
+				}
+			}
+		}
+
+	}
+}
