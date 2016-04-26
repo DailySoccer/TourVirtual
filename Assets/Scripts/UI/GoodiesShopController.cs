3ee8e75f70be3e45582bbc451733fbc763490456
sreveloc <sreveloc@gmail.com>
2016-04-26 11:21:32 +0200

    GoodiesShopController

---
+++ Assets/Scripts/UI/GoodiesShopController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodiesShopController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Product_ClickHandle(string iapId) {
+
+	}
+}
