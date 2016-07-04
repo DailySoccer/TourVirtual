2958be754bda0d54f065e34d3bf7b4ae3bbfef1d
sreveloc <sreveloc@gmail.com>
2016-07-04 17:02:26 +0200

    deeplinking + FB

--- Assets/Plugins/Microsoft/iOS/DeepLinking.mm
+++ Assets/Plugins/Microsoft/iOS/DeepLinking.mm
@@ -10,9 +10,12 @@
 @implementation MyAppController
 
 -(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
-    
-    UnitySendMessage("Azure Services", "SetDeepLinking", [[url absoluteString] cStringUsingEncoding:[NSString defaultCStringEncoding]] );
-    return YES;
+    // OJO que llegara el tema del SS.
+    if( [[url scheme] isEqualToString:@"rmvt"] ){
+        UnitySendMessage("Azure Services", "SetDeepLinking", [[url absoluteString] cStringUsingEncoding:[NSString defaultCStringEncoding]] );
+        return YES;
+    }
+    return NO;
 }
 @end
 
