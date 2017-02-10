48016ee63caaa9040663ea495d42a079e06586ac
Fer @Work <frs@unusualwonder.com>
2017-02-10 13:31:02 +0100

    [SDK] Update

---
+++ Assets/Scripts/Game/DeepLinkingManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DeepLinkingManager : Photon.PunBehaviour {
+    public static bool IsEditAvatar;
+    public static string URL;
+    public static Dictionary<string, object> Parameters = new Dictionary<string, object>();
+
+// He quitado esto porque hace que se logue n veces.
+        
+    void OnApplicationFocus(bool focusStatus) {
+        if (focusStatus ) {
+            var url = Authentication.AzureServices.CheckDeepLinking();
+            if(!string.IsNullOrEmpty(url)){
+                Debug.LogError(">>>>> SetDeepLinking ---> "+url);
+                DeepLinkingManager.IsEditAvatar = false;
+                if (string.IsNullOrEmpty(url)) return;
+                System.Uri uri = new System.Uri(url);
+
+                url = WWW.UnEscapeURL(url);
+                var pair = url.Split('?');
+
+                DeepLinkingManager.Parameters.Clear();
+                if (pair.Length == 2) {
+                    var pars = pair[1].Split('&');
+                    foreach (var p in pars) {
+                        var par = p.Split('=');
+                        if (par.Length == 2) {
+                            DeepLinkingManager.Parameters.Add(par[0], par[1]);
+                        }
+                    }
+                }
+
+                // Miro, primero que este logado...
+                if(!string.IsNullOrEmpty(UserAPI.Instance.UserID) && UserAPI.Instance.CheckIsOtherUser() ){
+                    // Mostrar la ventana de carga
+                    StartCoroutine(CanvasRootController.Instance.FadeOut(2));
+                    Debug.LogError(">>>> Es otro usuario ");
+                    return;
+                }
+
+                // Mira si no esta editando el avatar.
+                if(uri.Host!="editavatar"){
+//                    if (RoomManager.Instance.Room.Id != "VESTIDORLITE") RoomManager.Instance.GotoRoom("VESTIDORLITE");
+                    return;
+                }
+
+                DeepLinkingManager.IsEditAvatar = true;
+                DeepLinkingManager.URL = url;
+                Debug.LogError(">>>> OnDeepLinking");
+                // Tengo que ver para que viene el deeplinking.
+                if (RoomManager.Instance != null && UserAPI.Instance.Online && !UserAPI.Instance.errorLogin ) {
+                    if (RoomManager.Instance.Room.Id != "VESTIDORLITE") RoomManager.Instance.GotoRoom("VESTIDORLITE");
+                    else FindObjectOfType<VestidorCanvasController_Lite>().ShowClothesShop();
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/ModalContents.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModalContents : MonoBehaviour {
+
+	public static ModalContents Instance{ get; private set;}
+	public GUIPopUpScreen ModalScreen;
+	// Use this for initialization
+
+	void Awake(){
+		Instance = this;
+	}
+
+	public void ShowModalScreen(int newModalLayout)
+	{
+
+		if (ModalScreen == null) {
+//			Debug.LogError("[CanvasManager in " + name + "]: La guiModalScreen es null. Quizás no has establecido la primera desde el inspector.");
+			return;
+		}
+
+		// Lanzamos la modal, solo si está cerrada previamente.
+		HideModalScreen ();
+		if(((ModalLayout) newModalLayout!=ModalLayout.SETTINGS && (ModalLayout)newModalLayout!=ModalLayout.BLANK))
+			if( Authentication.Instance.CheckOffline()) 
+				return;
+
+		StartCoroutine (ModalCloseBeforeOpenAgain(newModalLayout));
+	}
+
+	IEnumerator ModalCloseBeforeOpenAgain(int newModalLayout) {
+
+		while (ModalScreen.InOpenState) {
+			yield return null;
+		}
+
+		ModalScreen.IsOpen = true;
+		ModalScreen.GetComponent<CanvasGroup>().interactable = true;
+		
+		PopUpWindow modalPopUpWindow = ModalScreen.GetComponent<PopUpWindow> ();
+		modalPopUpWindow.CurrentModalLayout = (ModalLayout)newModalLayout;
+	}
+
+	public void HideModalScreen() {
+		//if (ModalScreen != null) {
+		ModalScreen.IsOpen = false;
+		ModalScreen.GetComponent<CanvasGroup> ().interactable = false;
+		ModalScreen.GetComponent<PopUpWindow>().SetState(ModalLayout.BLANK);
+		//}
+	}
+
+	public void OpenOptions(){
+		ModalContents.Instance.ShowModalScreen(10);
+	}
+	
+}
