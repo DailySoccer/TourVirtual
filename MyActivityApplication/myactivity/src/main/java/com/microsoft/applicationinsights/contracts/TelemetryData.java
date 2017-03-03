f8b35ce06ccd57b906b024dba0302c8b19803c0b
Fernando Pérez <gunderwulde@gmail.com>
2017-03-03 17:20:00 +0100

    [MyActivity] Removed from project

--- MyActivityApplication/myactivity/src/main/java/com/microsoft/applicationinsights/contracts/TelemetryData.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.applicationinsights.contracts;
-
-import com.microsoft.telemetry.ITelemetry;
-
-import java.io.Serializable;
-
-public abstract class TelemetryData extends ITelemetry implements Serializable {}
--- MyActivityApplication/myactivity/src/main/java/com/microsoft/applicationinsights/library/ChannelType.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.applicationinsights.library;
-
-/**
- * An enum for the different channel types supported
- */
-public enum ChannelType {
-    Default,
-    CommonLoggingLibraryChannel
-}
