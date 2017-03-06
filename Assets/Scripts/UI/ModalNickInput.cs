a83a38e8bec995e43d89c9f00dae8c38fa987cd2
sreveloc <sreveloc@gmail.com>
2017-03-06 16:53:49 +0100

    ventana de input nick, feedback boton aceptar nick

--- Assets/Scripts/UI/ModalNickInput.cs
+++ Assets/Scripts/UI/ModalNickInput.cs
@@ -3,22 +3,27 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ModalNickInput : MonoBehaviour {
-
-
+	
+	
 	public Text TheNick;
 	public Button ButtonOK;
+	Text ButtonOKText;
+	Color ButtonOKTextColor;
 	GUIPopUpScreen thisModal;
-    public delegate void callback(string _nick);
-
+	public delegate void callback(string _nick);
+	
 	public static ModalNickInput Instance { get; private set; }
-
+	
 	void Awake () {
 		Instance = this;
 		thisModal = GetComponent<GUIPopUpScreen> ();
 		thisModal.IsOpen = false;
 		ButtonOK.enabled = false;
+		ButtonOKText = ButtonOK.transform.GetChild(0).GetComponent<Text>();
+		ButtonOKTextColor = ButtonOKText.color;
+		EvaluateNick();
 	}
-
+	
 	// Use this for initialization
 	void Start () {
 	}
@@ -27,29 +32,30 @@ public class ModalNickInput : MonoBehaviour {
 	void Update () {	
 	}
 
-    callback okCallback;
-
+	callback okCallback;
+	
 	public void EvaluateNick() {
 		Instance.ButtonOK.enabled = Instance.TheNick.text.Length >= 3;
+		Instance.ButtonOKText.color = Instance.TheNick.text.Length >= 3 ? ButtonOKTextColor : new Color(ButtonOKTextColor.r, ButtonOKTextColor.g, ButtonOKTextColor.b, ButtonOK.colors.disabledColor.a);
 	}
-
+	
 	public void AcceptNick() {
-        if (Instance.okCallback != null) Instance.okCallback(Instance.TheNick.text);
+		if (Instance.okCallback != null) Instance.okCallback(Instance.TheNick.text);
 	}
-
+	
 	public void SelfClose() {
-        if (Instance.okCallback != null) Instance.okCallback("<EMPTY>");
-        Close ();
+		if (Instance.okCallback != null) Instance.okCallback("<EMPTY>");
+		Close ();
 	}
-
-    public static void Show(callback _callback=null) {
+	
+	public static void Show(callback _callback=null) {
 		Instance.TheNick.text = UserAPI.Instance.Nick;
 		Instance.TheNick.transform.parent.GetComponent<InputField>().text =Instance.TheNick.text; 
 		Instance.okCallback = _callback;
 		Instance.thisModal.IsOpen = true;
 	}
-
+	
 	public static void Close() {
-        Instance.thisModal.IsOpen = false;
+		Instance.thisModal.IsOpen = false;
 	}
 }
