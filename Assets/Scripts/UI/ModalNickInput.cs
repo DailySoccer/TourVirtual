927ddaaf3d6f269164a19bc3cca231ae634d0c41
sreveloc <sreveloc@gmail.com>
2017-03-10 15:11:36 +0100

    botón ModalNickInput

--- Assets/Scripts/UI/ModalNickInput.cs
+++ Assets/Scripts/UI/ModalNickInput.cs
@@ -3,58 +3,64 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ModalNickInput : MonoBehaviour {
-	
-	
+
+
 	public Text TheNick;
 	public Button ButtonOK;
-	Text ButtonOKText;
-	Color ButtonOKTextColor;
+	Text buttonOKText;
+	Color buttonOKTextColor;
+	Color colorDisabled;
 	GUIPopUpScreen thisModal;
 	public delegate void callback(string _nick);
-	
+
 	public static ModalNickInput Instance { get; private set; }
-	
+
+	callback okCallback;
+
 	void Awake () {
 		Instance = this;
 		thisModal = GetComponent<GUIPopUpScreen> ();
 		thisModal.IsOpen = false;
-		ButtonOK.enabled = false;
-		ButtonOKText = ButtonOK.transform.GetChild(0).GetComponent<Text>();
-		ButtonOKTextColor = ButtonOKText.color;
+		ButtonOK.interactable = false;
+		buttonOKText = ButtonOK.transform.GetChild(0).GetComponent<Text>();
+		buttonOKTextColor = buttonOKText.color;
+		colorDisabled = new Color (buttonOKTextColor.r, buttonOKTextColor.g, buttonOKTextColor.b, ButtonOK.colors.disabledColor.a);
 		EvaluateNick();
 	}
-	
+
 	// Use this for initialization
 	void Start () {
 	}
-	
+
 	// Update is called once per frame
 	void Update () {	
 	}
-	
-	callback okCallback;
-	
+
 	public void EvaluateNick() {
-		Instance.ButtonOK.enabled = Instance.TheNick.text.Length >= 3;
-		Instance.ButtonOKText.color = Instance.TheNick.text.Length >= 3 ? ButtonOKTextColor : new Color(ButtonOKTextColor.r, ButtonOKTextColor.g, ButtonOKTextColor.b, ButtonOK.colors.disabledColor.a);
+		Instance.ButtonOK.interactable = Instance.TheNick.text.Length >= 3;
+		if (Instance.TheNick.text.Length >= 3) {
+			Instance.buttonOKText.color =  buttonOKTextColor;
+		} else {
+			Instance.buttonOKText.color =  colorDisabled;
+		}
 	}
-	
+
 	public void AcceptNick() {
 		if (Instance.okCallback != null) Instance.okCallback(Instance.TheNick.text);
 	}
-	
+
 	public void SelfClose() {
 		if (Instance.okCallback != null) Instance.okCallback("<EMPTY>");
 		Close ();
 	}
-	
+
 	public static void Show(callback _callback=null) {
 		Instance.TheNick.text = UserAPI.Instance.Nick;
 		Instance.TheNick.transform.parent.GetComponent<InputField>().text =Instance.TheNick.text; 
 		Instance.okCallback = _callback;
 		Instance.thisModal.IsOpen = true;
 	}
-	
+
 	public static void Close() {
 		Instance.thisModal.IsOpen = false;
 	}
