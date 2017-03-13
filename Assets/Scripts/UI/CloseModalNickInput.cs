2c4f4ad8f652e02e4558214615c1a42dc2a8f9cc
sreveloc <sreveloc@gmail.com>
2017-03-13 23:38:44 +0100

    fix validación nick input

--- Assets/Scripts/UI/CloseModalNickInput.cs
+++ Assets/Scripts/UI/CloseModalNickInput.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CloseModalNickInput: MonoBehaviour {
 	public void CloseModal() {
+		ModalNickInput.Instance.EvaluateNick ();
 		if (ModalNickInput.Instance.TheNick.text.Length >= 3)
 			ModalNickInput.Instance.AcceptNick ();
 	}
