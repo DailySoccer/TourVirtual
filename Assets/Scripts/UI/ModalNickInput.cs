365f8f1224e89b54c1f269fefebba02bf0cfe6ea
sreveloc <sreveloc@gmail.com>
2017-03-21 15:58:55 +0100

    Reactivacion de botón de modalnickiput con coloreado para activado/desactivado

--- Assets/Scripts/UI/ModalNickInput.cs
+++ Assets/Scripts/UI/ModalNickInput.cs
@@ -6,10 +6,17 @@ public class ModalNickInput : MonoBehaviour {
 	
 	
 	public Text TheNick;
+
 	public Button ButtonOK;
+
+	Image buttonOKImage;
+	Color buttonOKImageColorActive;
+	Color buttonOKImageColorDisabled;
+
 	Text buttonOKText;
-	Color buttonOKTextColor;
-	Color colorDisabled;
+	Color buttonOKTextColorActive;
+	Color buttonOKTextColorDisabled;
+
 	GUIPopUpScreen thisModal;
 	public delegate void callback(string _nick);
 	
@@ -21,10 +28,18 @@ public class ModalNickInput : MonoBehaviour {
 		Instance = this;
 		thisModal = GetComponent<GUIPopUpScreen> ();
 		thisModal.IsOpen = false;
-		ButtonOK.gameObject.SetActive(false);
-		//buttonOKText = ButtonOK.transform.GetChild(0).GetComponent<Text>();		
-		//buttonOKTextColor = buttonOKText.color;
-		//colorDisabled = new Color (buttonOKTextColor.r, buttonOKTextColor.g, buttonOKTextColor.b, ButtonOK.colors.disabledColor.a);
+
+		//ButtonOK.gameObject.SetActive(false);
+
+		buttonOKImage 				= ButtonOK.GetComponent<Image>();		
+		buttonOKImageColorActive 	= new Color (1f, 1f, 1f, 1f);
+		buttonOKImageColorDisabled 	= new Color (0.6f, 0.6f, 0.6f, 0.6f);
+
+		buttonOKText 				= ButtonOK.transform.GetChild(0).GetComponent<Text>();		
+		buttonOKTextColorActive 	= new Color (21f/255f, 28f/255f, 43f/255f, 1.0f);
+		buttonOKTextColorDisabled 	= new Color (buttonOKTextColorActive.r, buttonOKTextColorActive.g, buttonOKTextColorActive.b, 0.6f);
+
+		Instance.ButtonOK.interactable = true;
 	}
 	
 	// Use this for initialization
@@ -33,12 +48,12 @@ public class ModalNickInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//buttonOKText.color =  colorDisabled;
-		//ButtonOK.image.color = colorDisabled;
+		EvaluateNick();
 	}
 	
 	public void EvaluateNick() {
-		//Instance.ButtonOK.interactable = Instance.TheNick.text.Length >= 3;
+		buttonOKImage.color = (Instance.TheNick.text.Length >= 3) ? buttonOKImageColorActive : buttonOKImageColorDisabled;
+		buttonOKText.color  = (Instance.TheNick.text.Length >= 3) ? buttonOKTextColorActive  : buttonOKTextColorDisabled;
 	}
 	
 	public void AcceptNick() {
