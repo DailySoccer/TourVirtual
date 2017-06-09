feb1bc43b177f1e533a4935cd91b0458964b6acc
Ximo <xmalbors@hotmail.com>
2017-06-09 17:46:58 +0200

    fix Analytics: + ChatsData

---
+++ Assets/Scripts/Analytics/ChatsData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class ChatsData {
+
+    public int TotalPrivateMessages;
+    private HashSet<string> _users;
+
+    public int TotalUsers {
+        get {
+            return _users.Count;
+        }
+    }
+
+    public void Reset() {
+        TotalPrivateMessages = 0;
+        _users = new HashSet<string>();
+    }
+
+    public void PrivateMessage(string userId) {
+        TotalPrivateMessages++;
+        _users.Add(userId);
+    }
+}
